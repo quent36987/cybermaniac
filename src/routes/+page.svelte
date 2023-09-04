4866e1692d6dbe5a69a6ddb358ea53524f7a4166
Quentin Goujon <quentin.goujon@epita.fr>
2023-09-04 09:11:35 +0200

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,233 @@
+<script>
+	let activeIndex = -1;
+	let cardElements = [];
+	let dots = [];
+	const services = [
+		{
+			name: 'Entretiens de vos équipements',
+			description: "Expertise en entretien informatique avec devis gratuit. Votre satisfaction est notre priorité.",
+			image: 'src/lib/entretien.jpeg'
+		},
+		{
+			name: 'Vente Informatique',
+			description: "Large gamme d'ordinateurs, du bureautique au gaming et configurations personnalisées.",
+			image: 'src/lib/vente.png'
+		},
+		{
+			name: 'Déplacement à domicile',
+			description: "Partenariat avec Frozeyes IMR pour des interventions à domicile. Solutions rapides par des experts.",
+			image: 'src/lib/domicil.jpg'
+		},
+		{
+			name: 'Impression 3D FDM',
+			description: "Réparation d'objets et création personnalisée grâce à l'impression 3D.",
+			image: 'src/lib/impression.jpg'
+		},
+		{
+			name: 'Impression 3D Résine',
+			description: 'Créations de précision avec impression en résine pour vos projets.',
+			image: 'src/lib/resine.jpg'
+		},
+		{
+			name: 'Modélisation Technique',
+			description: "Redonnez vie à vos objets et plans avec notre service de modélisation 3D.",
+			image: 'src/lib/modelisation.jpg'
+		},
+		{
+			name: 'Matériaux',
+			description: "Conseils sur les matériaux adaptés et vente de matières premières pour l'impression 3D.",
+			image: 'src/lib/materiaux.jpg'
+		}
+	];
+
+	function setActive(index) {
+		activeIndex = index;
+	}
+
+	function setActiveAndCenter(index) {
+		setActive(index);
+
+		const card = cardElements[index];
+		const carousel = document.querySelector('.carousel');
+		const rect = card.getBoundingClientRect();
+		const carouselRect = carousel.getBoundingClientRect();
+		const scrollLeft =
+			rect.left - carouselRect.left + carousel.scrollLeft - (carousel.clientWidth - rect.width) / 2;
+
+		carousel.scrollTo({
+			left: scrollLeft,
+			behavior: 'smooth'
+		});
+	}
+
+	function centerCard(event) {
+		const card = event.currentTarget;
+		const carousel = document.querySelector('.carousel');
+		const rect = card.getBoundingClientRect();
+		const carouselRect = carousel.getBoundingClientRect();
+		const scrollLeft =
+			rect.left - carouselRect.left + carousel.scrollLeft - (carousel.clientWidth - rect.width) / 2;
+
+		carousel.scrollTo({
+			left: scrollLeft,
+			behavior: 'smooth'
+		});
+	}
+</script>
+
+<section class="game-section">
+	<h2 class="line-title">Nos services</h2>
+	<div class="carousel">
+		{#each services as game, index}
+			<div
+				class="item {activeIndex === index ? 'active' : ''}"
+				style="background-image: url({game.image});"
+				on:click={(e) => {
+					centerCard(e);
+					setActive(index);
+				}}
+				bind:this={cardElements[index]}
+			>
+				<div class="item-desc">
+					<h3>{game.name}</h3>
+					<p>{game.description}</p>
+					<div class="link">En savoir plus</div>
+				</div>
+			</div>
+		{/each}
+	</div>
+	<div class="carousel-dots">
+		{#each services as card, index}
+			<span
+				class="dot {activeIndex === index ? 'active' : ''}"
+				on:click={() => setActiveAndCenter(index)}
+				bind:this={dots[index]}
+			/>
+		{/each}
+	</div>
+</section>
+
+<style>
+	/* Title style */
+	.line-title {
+		font-family: Century Gothic, Arial, sans-serif;
+		font-size: 30px;
+	}
+	.line-title::before,
+	.line-title::after {
+		content: '';
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		height: 4px;
+		border-radius: 2px;
+	}
+	.line-title::before {
+		width: 100%;
+		background: #f2f2f2;
+	}
+	.line-title::after {
+		width: 50%;
+		background: #0064e7;
+	}
+
+	/* Middle section CSS */
+	.game-section {
+		padding: 60px 50px;
+	}
+
+	.carousel {
+		display: flex;
+		gap: 16px; /* Espacement entre les cartes */
+		scroll-snap-type: x mandatory;
+		overflow-x: hidden;
+		scrollbar-width: none;
+		font-family: Century Gothic, Arial, sans-serif;
+	}
+	.item {
+		margin: 0 15px 30px;
+		width: 320px;
+		height: 400px;
+		display: flex;
+		align-items: flex-end;
+		background: #343434 no-repeat center center / cover;
+		border-radius: 16px;
+		overflow: hidden;
+		position: relative;
+		transition: all 0.4s ease-in-out;
+		cursor: pointer;
+		flex: 0 0 auto;
+		scroll-snap-align: start;
+	}
+	.item.active {
+		width: 500px;
+		box-shadow: 12px 20px 20px rgba(0, 0, 0, 0.25);
+	}
+	.item:after {
+		content: '';
+		display: block;
+		position: absolute;
+		height: 100%;
+		width: 100%;
+		left: 0;
+		top: 0;
+		background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1));
+	}
+	.item-desc {
+		padding: 0 24px 24px; /* Ajouté plus de padding en bas */
+		color: #fff;
+		position: relative;
+		z-index: 1;
+		overflow: hidden;
+		transform: translateY(calc(100% - 100px));
+		transition: all 0.4s ease-in-out;
+	}
+
+	.item-desc h3 {
+		font-size: 1.5em; /* Augmenté la taille de la police */
+		margin-bottom: 10px; /* Espacement entre le titre et le texte de description */
+	}
+	.item.active .item-desc {
+		transform: none;
+	}
+	.item-desc p {
+		opacity: 0;
+		transform: translateY(32px);
+		transition: all 0.4s ease-in-out 0.2s;
+		font-weight: bold;
+	}
+	.item.active .item-desc p {
+		opacity: 1;
+		transform: translateY(0);
+	}
+
+	.link {
+		display: inline-block;
+		padding: 8px 16px;
+		background: #0064e7;
+		border-radius: 4px;
+		margin-top: 16px;
+		color: #fff;
+		font-weight: bold;
+		transition: all 0.4s ease-in-out;
+	}
+
+	.carousel-dots {
+		text-align: center;
+		margin-top: 10px;
+	}
+
+	.dot {
+		height: 10px;
+		width: 10px;
+		margin: 0 4px;
+		background-color: #bbb;
+		border-radius: 50%;
+		display: inline-block;
+		cursor: pointer;
+	}
+
+	.dot.active {
+		background-color: #333;
+	}
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,54 @@
+<script>
+	let services = [
+		'Entretiens de vos équipements',
+		'Vente Informatique',
+		'Déplacement à domicile',
+		'Impression 3D FDM',
+		'Impression 3D Résine',
+		'Modélisation Technique',
+		'Matériaux'
+	];
+</script>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1rem 5%;
+	}
+
+	nav {
+		display: flex;
+		gap: 1rem;
+	}
+
+	.dropdown-content {
+		display: none;
+		position: absolute;
+		background-color: white;
+		border: 1px solid #ddd;
+	}
+
+	.dropdown:hover .dropdown-content {
+		display: block;
+	}
+</style>
+
+<header>
+	<img src="src/lib/logo.png" alt="Company Logo" />
+
+	<nav>
+		<div class="dropdown">
+			Services
+			<div class="dropdown-content">
+				{#each services as service}
+					<a href="#">{service}</a>
+				{/each}
+			</div>
+		</div>
+		<a href="#">Boutique</a>
+		<a href="#">Plan d'accès</a>
+		<a href="#">Nous contacter</a>
+	</nav>
+</header>
