f77ea118e003f032a86107ee004c8b91a7cd5c30
Goujon Quentin <quentin.goujon@epita.fr>
2023-09-28 10:01:24 +0200

    test refacto

--- src/routes/about/domicile/+page.svelte
+++ src/routes/about/domicile/+page.svelte
@@ -1,82 +1,92 @@
 <script>
 </script>
 
-<div class="content-wrapper" >
-	<div class="title">Déplacement à domicile</div>
+<div class="content-wrapper">
+    <div class="title">Déplacement à domicile</div>
 
-	<div class="page">
-		<div class="desc">
-			Vous pouvez faire appel à notre service de déplacement à domicile pour tous types de problèmes informatiques. Nous intervenons sur les appareils Windows et Mac, et pour les utilisateurs de Mac, notre service se fait exclusivement à domicile.<br/><br/>
+    <div class="page">
+        <div class="desc">
+            Vous pouvez faire appel à notre service de déplacement à domicile pour tous types de problèmes
+            informatiques. Nous intervenons sur les appareils Windows et Mac, et pour les utilisateurs de Mac, notre
+            service se fait exclusivement à domicile.<br/><br/>
 
-			Ce service est fourni en partenariat avec notre prestataire de confiance, <strong>Frozeyes IMR</strong>. Leur technicien qualifié se rendra chez vous pour résoudre vos problèmes informatiques de manière professionnelle et efficace.<br/><br/>
+            Ce service est fourni en partenariat avec notre prestataire de confiance, <strong>Frozeyes IMR</strong>.
+            Leur technicien qualifié se rendra chez vous pour résoudre vos problèmes informatiques de manière
+            professionnelle et efficace.<br/><br/>
 
-			Pour prendre rendez-vous, vous pouvez nous contacter par téléphone au <strong>06 84 25 25 27</strong> ou au <strong>06 95 84 14 57</strong>. Chez CyberManiac, nous comprenons l'importance d'une assistance informatique à domicile pour résoudre rapidement vos problèmes.<br/><br/>
+            Pour prendre rendez-vous, vous pouvez nous contacter par téléphone au <strong>06 84 25 25 27</strong> ou au
+            <strong>06 95 84 14 57</strong>. Chez CyberManiac, nous comprenons l'importance d'une assistance
+            informatique à domicile pour résoudre rapidement vos problèmes.<br/><br/>
 
-			N'hésitez pas à solliciter notre service de déplacement à domicile et faites confiance à notre équipe expérimentée pour résoudre vos problèmes informatiques.
-		</div>
+            N'hésitez pas à solliciter notre service de déplacement à domicile et faites confiance à notre équipe
+            expérimentée pour résoudre vos problèmes informatiques.
+        </div>
 
-		<div class="images">
-			<img class="image" src="/about/domicile/image1.png" alt="Déplacement à domicile" />
-		</div>
-	</div>
+        <div class="images">
+            <img class="image" src="/about/domicile/image1.png" alt="Déplacement à domicile"/>
+        </div>
+    </div>
 </div>
 
 <style>
-	.content-wrapper {
-		max-width: 1200px;
-		margin: auto;
-		padding: var(--spacing-l);
-	}
+    .content-wrapper {
+        max-width: 1200px;
+        margin: auto;
+        padding: var(--spacing-l);
+    }
 
-	.title {
-		font-family: 'Century Gothic', Arial, sans-serif;
-		font-size: 1.3rem;
-		padding-bottom: var(--spacing-l);
-		border-bottom: 3px solid #E0E0E0;
-		margin-bottom: var(--spacing-l);
-	}
+    .title {
+        font-family: 'Century Gothic', Arial, sans-serif;
+        font-size: 30px;
+        padding-bottom: var(--spacing-l);
+        border-bottom: 3px solid #E0E0E0;
+        margin-bottom: var(--spacing-l);
+    }
 
-	.page {
-		display: flex;
-		justify-content: space-between;
-		gap: var(--spacing-l);
-	}
+    .page {
+        display: flex;
+        justify-content: space-between;
+        gap: var(--spacing-l);
+    }
 
-	.desc {
-		flex: 2.5;
-		line-height: 1.6;
-		font-size: 1rem;
-	}
+    .desc {
+        flex: 2.5;
+        line-height: 1.6;
+        font-size: 1rem;
+    }
 
-	.desc strong {
-		color: #333;
-		font-weight: bold;
-	}
+    .desc strong {
+        color: #333;
+        font-weight: bold;
+    }
 
-	.images {
-		flex: 1;
-	}
+    .images {
+        flex: 1;
+    }
 
-	.image {
-		max-width: 100%;
-		display: block;
-		box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-		border-radius: 5px;
-		transition: transform 0.3s ease;
-	}
+    .image {
+        max-width: 100%;
+        display: block;
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+        border-radius: 5px;
+        transition: transform 0.3s ease;
+    }
 
-	.image:hover {
-		transform: scale(1.05);
-	}
-	@media screen and (max-width: 500px) {
-		.title {
-			font-size: 1.2rem;
-		}
-		.content-wrapper {
-			padding: var(--spacing-m);
-		}
-		.desc {
-			font-size: 0.8rem;
-		}
-	}
+    .image:hover {
+        transform: scale(1.05);
+    }
+
+    @media screen and (max-width: 500px) {
+        .title {
+            font-size: 1.2rem;
+        }
+
+        .content-wrapper {
+            padding: var(--spacing-m);
+        }
+
+        .desc {
+            font-size: 0.8rem;
+        }
+    }
 </style>
--- src/routes/about/vente/+page.svelte
+++ src/routes/about/vente/+page.svelte
@@ -1,285 +1,271 @@
 <script>
-	// Votre script JavaScript si nécessaire
+    // Votre script JavaScript si nécessaire
 </script>
 
 <div class="content-wrapper">
-	<div class="section">
-		<div class="title">Entretiens de vos équipements</div>
+    <div class="section">
+        <div class="title">Entretiens de vos équipements</div>
 
-		<div class="page">
-			<div class="desc">
-				Notre équipe de professionnels qualifiés est disponible pour offrir des conseils et des
-				recommandations pour aider nos clients à trouver les produits et les services qui
-				conviennent le mieux à leurs besoins et à leur budget, une livraison rapide et fiable.
-			</div>
+        <div class="page">
+            <div class="desc">
+                Notre équipe de professionnels qualifiés est disponible pour offrir des conseils et des
+                recommandations pour aider nos clients à trouver les produits et les services qui
+                conviennent le mieux à leurs besoins et à leur budget, une livraison rapide et fiable.
+            </div>
 
-			<div class="images">
-				<img class="image" src="/about/vente/image1.jpg" alt="Entretiens d'imprimantes" />
-			</div>
-		</div>
-	</div>
+            <div class="images">
+                <img class="image" src="/about/vente/image1.jpg" alt="Entretiens d'imprimantes"/>
+            </div>
+        </div>
+    </div>
 
-	<div class="section">
-		<div class="title">Ordinateurs Fixe</div>
+    <div class="section">
+        <div class="title">Ordinateurs Fixe</div>
 
-		<div class="page">
-			<div class="images">
-				<img class="image" src="/about/vente/image2.jpg" alt="Entretiens d'imprimantes" />
-			</div>
-			<div class="desc">
-				Chez CyberManiac, nous sommes fiers de proposer un service de montage de PC fixe
-				spécialement conçu pour répondre à vos besoins en bureautique, en offrant le meilleur
-				rapport qualité-prix possible.
-				<br /><br />
+        <div class="page">
+            <div class="images">
+                <img class="image" src="/about/vente/image2.jpg" alt="Entretiens d'imprimantes"/>
+            </div>
+            <div class="desc">
+                Chez CyberManiac, nous sommes fiers de proposer un service de montage de PC fixe
+                spécialement conçu pour répondre à vos besoins en bureautique, en offrant le meilleur
+                rapport qualité-prix possible.
+                <br/><br/>
 
-				Notre équipe est dévouée à vous fournir une configuration qui correspondra parfaitement à
-				vos attentes. Nous tenons compte de vos exigences spécifiques en termes de performances, de
-				fiabilité et de budget afin de vous offrir la solution la plus adaptée.
-			</div>
-		</div>
-	</div>
+                Notre équipe est dévouée à vous fournir une configuration qui correspondra parfaitement à
+                vos attentes. Nous tenons compte de vos exigences spécifiques en termes de performances, de
+                fiabilité et de budget afin de vous offrir la solution la plus adaptée.
+            </div>
+        </div>
+    </div>
 
-	<div class="section">
-		<div class="title">Montage sur mesure</div>
-		<p class="description">
-			Nous attachons une grande importance à la qualité de notre service de montage. Chaque PC est
-			assemblé avec soin et rigueur, en veillant à ce que tous les composants soient correctement
-			installés et configurés. Nous effectuons des tests approfondis pour nous assurer que votre PC
-			est prêt à affronter les défis du gaming sans compromettre la stabilité et les performances.
-			Faites confiance à CyberManiac pour obtenir une configuration de PC gaming adaptée à vos
-			besoins, offrant des performances exceptionnelles tout en respectant votre budget. Nous sommes
-			passionnés par les jeux vidéo et nous nous engageons à vous offrir la meilleure expérience de
-			jeu possible.
-		</p>
-		<div class="image-gallery">
-			<div class="img-wrapper">
-				<img src="/about/vente/image4.jpg" alt="Image 4" />
-			</div>
-			<div class="img-wrapper">
-				<img src="/about/vente/image11.jpg" alt="Image 11" />
-			</div>
-			<div class="img-wrapper">
-				<img src="/about/vente/image5.jpg" alt="Image 5" />
-			</div>
-			<div class="img-wrapper">
-				<img src="/about/vente/image10.jpg" alt="Image 10" />
-			</div>
+    <div class="section">
+        <div class="title">Montage sur mesure</div>
+        <p class="description">
+            Nous valorisons la qualité de notre assemblage de PC, assurant l'installation et la configuration précises
+            des composants et effectuant des tests rigoureux pour garantir performances et stabilité pour le gaming.
+            Chez CyberManiac, nous vous fournissons une configuration de PC gaming adaptée à vos besoins et budget, tout
+            en visant une expérience de jeu optimale.
+        </p>
+        <div class="image-gallery">
+            <div class="img-wrapper">
+                <img src="/about/vente/image4.jpg" alt="Image 4"/>
+            </div>
+            <div class="img-wrapper">
+                <img src="/about/vente/image11.jpg" alt="Image 11"/>
+            </div>
+            <div class="img-wrapper">
+                <img src="/about/vente/image5.jpg" alt="Image 5"/>
+            </div>
+            <div class="img-wrapper">
+                <img src="/about/vente/image10.jpg" alt="Image 10"/>
+            </div>
 
-			<div class="img-wrapper">
-				<img src="/about/vente/image7.jpg" alt="Image 7" />
-			</div>
-			<div class="img-wrapper">
-				<img src="/about/vente/image8.jpg" alt="Image 8" />
-			</div>
-			<div class="img-wrapper">
-				<img src="/about/vente/image9.jpg" alt="Image 9" />
-			</div>
-			<div class="img-wrapper">
-				<img src="/about/vente/image6.jpg" alt="Image 6" />
-			</div>
-		</div>
-	</div>
+            <div class="img-wrapper">
+                <img src="/about/vente/image7.jpg" alt="Image 7"/>
+            </div>
+            <div class="img-wrapper">
+                <img src="/about/vente/image8.jpg" alt="Image 8"/>
+            </div>
+            <div class="img-wrapper">
+                <img src="/about/vente/image9.jpg" alt="Image 9"/>
+            </div>
+            <div class="img-wrapper">
+                <img src="/about/vente/image6.jpg" alt="Image 6"/>
+            </div>
+        </div>
+    </div>
 
-	<div class="section">
-		<div class="title">Ordinateurs portable</div>
-		<p class="description">
-			Nous proposons également une sélection de PC portables de marques renommées telles qu'Asus et
-			Lenovo, avec des tailles d'écran allant de 15 à 17 pouces. Chez nous, vous trouverez une gamme
-			de PC portables soigneusement choisie pour répondre à vos besoins spécifiques. Que vous soyez
-			un étudiant, un professionnel ou un passionné de jeux vidéo, nous avons des options adaptées à
-			chaque utilisation. Faites confiance à notre expertise pour trouver le PC portable qui
-			répondra à vos attentes en matière de mobilité, de performances et de qualité de construction.
-			Nous sommes là pour vous aider à trouver l'appareil parfait pour vos besoins, que ce soit pour
-			travailler, étudier ou vous divertir.
-		</p>
-		<div class="image-gallery">
-			<div class="img-wrapper">
-				<img src="/about/vente/image12.jpg" alt="Image 4" />
-			</div>
-			<div class="img-wrapper">
-				<img src="/about/vente/image13.jpg" alt="Image 11" />
-			</div>
-		</div>
-	</div>
+    <div class="section">
+        <div class="title">Ordinateurs portable</div>
+        <p class="description">
+            Nous proposons principalement des PC portables de la marque Tera, reconnue pour sa qualité et offrant des
+            appareils de très bonne qualité. Chez nous, vous trouverez une sélection de PC portables Tera adaptés à
+            divers besoins, que vous soyez étudiant, professionnel ou gamer. Faites confiance à notre expertise pour
+            vous aider à trouver le PC portable idéal pour travailler, étudier ou vous divertir, répondant à vos
+            attentes en matière de mobilité, performances et qualité de construction.
+        </p>
+        <div class="image-gallery">
+            <div class="img-wrapper">
+                <img src="/about/vente/image12.jpg" alt="Image 4"/>
+            </div>
+            <div class="img-wrapper">
+                <img src="/about/vente/image13.jpg" alt="Image 11"/>
+            </div>
+            <div class="img-wrapper">
+                <img src="/about/vente/image24.jpg" alt="Image 24"/>
+            </div>
+        </div>
+    </div>
 
-	<div class="section">
-		<div class="title">Peripheriques</div>
+    <div class="section">
+        <div class="title">Peripheriques</div>
 
-		<div class="page">
-			<div class="desc">
-				Chez CyberManiac, nous sommes bien plus que de simples experts en montage d'ordinateurs.
-				Nous vous offrons une gamme complète d'accessoires, allant des souris sans fil, casques,
-				clés Wi-Fi, jusqu'aux disques durs et adaptateurs, conçus pour améliorer et personnaliser
-				votre expérience informatique.
-				<br/><br/>
-				Quels que soient vos besoins - travail, loisirs ou gaming -
-				notre équipe dédiée est là pour vous conseiller et vous aider à sélectionner des accessoires
-				de qualité. Fiez-vous à CyberManiac pour optimiser l'utilisation de votre ordinateur et vous
-				accompagner dans chaque étape technologique.
-			</div>
+        <div class="page">
+            <div class="desc">
+                Chez CyberManiac, nous proposons une variété d'accessoires pour améliorer votre expérience informatique,
+                qu'il s'agisse de travail, de loisirs ou de gaming. Notre équipe est là pour vous conseiller et vous
+                accompagner dans vos choix technologiques.
+            </div>
 
-			<div class="images">
-				<img class="image" src="/about/vente/image14.jpg" alt="Entretiens d'imprimantes" />
-			</div>
-		</div>
-		<div class="page">
-			<div class="images">
-				<img class="image" src="/about/vente/image15.jpg" alt="Entretiens d'imprimantes" />
-			</div>
-			<div class="desc">
-				Pour une navigation pratique et fluide, nous proposons des souris avec ou sans fil, adaptées
-				à vos préférences personnelles. Que vous recherchiez une précision accrue pour la
-				bureautique ou des fonctionnalités supplémentaires pour le gaming, nous avons des options
-				adaptées à chaque besoin.
-				<br /><br />
+            <div class="images">
+                <img class="image" src="/about/vente/image14.jpg" alt="Entretiens d'imprimantes"/>
+            </div>
+        </div>
+        <div class="page">
+            <div class="images">
+                <img class="image" src="/about/vente/image15.jpg" alt="Entretiens d'imprimantes"/>
+            </div>
+            <div class="desc">
+                Nous offrons des souris, avec ou sans fil, pour une navigation adaptée à vos besoins, que ce soit pour
+                la bureautique ou le gaming. Pour une expérience audio immersive, découvrez nos casques de qualité,
+                idéaux pour la musique, les jeux et les appels en ligne. De plus, nous proposons des clés Wi-Fi pour une
+                connexion Internet facile et sans fil.
+            </div>
+        </div>
+    </div>
 
-				Si vous souhaitez profiter d'une expérience audio immersive, nous avons une sélection de
-				casques de qualité. Que ce soit pour écouter de la musique, jouer à des jeux ou participer à
-				des appels en ligne, nos casques offrent un son clair et une grande confort d'utilisation.
-				<br /><br />
+    <div class="section">
+        <div class="title">Pièces</div>
+        <p class="description">
+            Nous proposons une large gamme de pièces d’ordinateur de marques renommées pour construire, mettre à niveau
+            ou réparer votre système. Notre sélection inclut des cartes mères, cartes graphiques, mémoire RAM, solutions
+            de stockage et plus. Notre équipe est là pour vous guider vers les composants essentiels pour une expérience
+            informatique optimale.
+        </p>
+        <div class="image-gallery">
+            <div class="img-wrapper">
+                <img src="/about/vente/image17.jpg" alt="Image 11"/>
+            </div>
+            <div class="img-wrapper">
+                <img src="/about/vente/image21.jpg" alt="Image 8"/>
+            </div>
+            <div class="img-wrapper">
+                <img src="/about/vente/image16.jpg" alt="Image 4"/>
+            </div>
+            <div class="img-wrapper">
+                <img src="/about/vente/image18.jpg" alt="Image 5"/>
+            </div>
+            <div class="img-wrapper">
+                <img src="/about/vente/image19.jpg" alt="Image 10"/>
+            </div>
 
-				Pour vous connecter sans fil à Internet, nous proposons des clés Wi-Fi. Elles vous
-				permettent de vous connecter facilement à votre réseau.
-			</div>
-		</div>
-	</div>
+            <div class="img-wrapper">
+                <img src="/about/vente/image20.jpg" alt="Image 7"/>
+            </div>
+            <div class="img-wrapper">
+                <img src="/about/vente/image23.jpg" alt="Image 6"/>
+            </div>
 
-	<div class="section">
-		<div class="title">Pièces</div>
-		<p class="description">
-			nous offrons une vaste gamme de pièces d'ordinateur de marques réputées, adaptées à vos
-			besoins spécifiques, que ce soit pour construire, mettre à niveau ou réparer. Notre sélection
-			englobe des cartes mères aux fonctionnalités avancées, des cartes graphiques, de la mémoire
-			RAM, des solutions de stockage, et plus encore. Que vous cherchiez une alimentation stable,
-			des solutions de refroidissement efficaces ou n'importe quel composant essentiel, notre équipe
-			dédiée est là pour vous guider et vous assurer une expérience informatique optimale.
-		</p>
-		<div class="image-gallery">
-			<div class="img-wrapper">
-				<img src="/about/vente/image17.jpg" alt="Image 11" />
-			</div>
-			<div class="img-wrapper">
-				<img src="/about/vente/image21.jpg" alt="Image 8" />
-			</div>
-			<div class="img-wrapper">
-				<img src="/about/vente/image16.jpg" alt="Image 4" />
-			</div>
-			<div class="img-wrapper">
-				<img src="/about/vente/image18.jpg" alt="Image 5" />
-			</div>
-			<div class="img-wrapper">
-				<img src="/about/vente/image19.jpg" alt="Image 10" />
-			</div>
-
-			<div class="img-wrapper">
-				<img src="/about/vente/image20.jpg" alt="Image 7" />
-			</div>
-			<div class="img-wrapper">
-				<img src="/about/vente/image23.jpg" alt="Image 6" />
-			</div>
-
-			<div class="img-wrapper">
-				<img src="/about/vente/image22.jpg" alt="Image 9" />
-			</div>
-		</div>
-	</div>
+            <div class="img-wrapper">
+                <img src="/about/vente/image22.jpg" alt="Image 9"/>
+            </div>
+        </div>
+    </div>
 </div>
 
 <style>
-	.content-wrapper {
-		max-width: 1200px;
-		margin: auto;
-		padding: var(--spacing-l);
-	}
+    .content-wrapper {
+        max-width: 1200px;
+        margin: auto;
+        padding: var(--spacing-l);
+    }
 
-	.page {
-		display: flex;
-		justify-content: space-between;
-		gap: var(--spacing-l);
-	}
+    .page {
+        display: flex;
+        justify-content: space-between;
+        gap: var(--spacing-l);
+    }
 
-	.desc {
-		flex: 2.5;
-		line-height: 1.6;
-		font-size: 18px;
-	}
+    .desc {
+        flex: 2.5;
+        line-height: 1.6;
+        font-size: 18px;
+    }
 
-	.desc strong {
-		color: #333;
-		font-weight: bold;
-	}
+    .desc strong {
+        color: #333;
+        font-weight: bold;
+    }
 
-	.images {
-		flex: 1;
-	}
+    .images {
+        flex: 1;
+    }
 
-	.image {
-		max-width: 100%;
-		display: block;
-		box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-		border-radius: 5px;
-		transition: transform 0.3s ease;
-	}
+    .image {
+        max-width: 100%;
+        display: block;
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+        border-radius: 5px;
+        transition: transform 0.3s ease;
+    }
 
-	.image:hover {
-		transform: scale(1.05);
-	}
+    .image:hover {
+        transform: scale(1.05);
+    }
 
-	.section {
-		margin-bottom: 4rem;
-	}
+    .section {
+        margin-bottom: 4rem;
+    }
 
-	.title {
-		font-family: 'Century Gothic', Arial, sans-serif;
-		font-size: 24px;
-		padding-bottom: 1rem;
-		border-bottom: 2px solid #e0e0e0;
-		margin-bottom: 1rem;
-	}
+    .title {
+        font-family: 'Century Gothic', Arial, sans-serif;
+        font-size: 24px;
+        padding-bottom: 1rem;
+        border-bottom: 2px solid #e0e0e0;
+        margin-bottom: 1rem;
+    }
 
-	.description {
-		margin-bottom: 1rem;
-		font-size: 18px;
-		line-height: 1.6;
-	}
+    .description {
+        margin-bottom: 1rem;
+        font-size: 18px;
+        line-height: 1.6;
+    }
 
-	img {
-		max-width: 100%;
-		display: block;
-		border-radius: var(--border-radius);
-	}
+    img {
+        max-width: 100%;
+        display: block;
+        border-radius: var(--border-radius);
+    }
 
-	.img-wrapper {
-		margin: 0;
-		break-inside: avoid;
-		margin-bottom: var(--spacing-s);
-	}
+    .img-wrapper {
+        margin: 0;
+        break-inside: avoid;
+        margin-bottom: var(--spacing-s);
+    }
 
-	.image-gallery {
-		column-count: 5; /* Pour avoir 5 images par ligne */
-		column-gap: var(--spacing-s);
-	}
-	@media screen and (max-width: 500px) {
-		.image-gallery {
-			column-count: 3;
-		}
-		.content-wrapper {
-			padding: var(--spacing-m);
-		}
-		.description {
-			font-size: 0.9rem;
-		}
-		.title {
-			font-size: 1.2rem;
-		}
-		.title {
-			font-size: 1.2rem;
-		}
-		.content-wrapper {
-			padding: var(--spacing-m);
-		}
-		.desc {
-			font-size: 0.8rem;
-		}
-	}
+    .image-gallery {
+        column-count: 5; /* Pour avoir 5 images par ligne */
+        column-gap: var(--spacing-s);
+    }
+
+    @media screen and (max-width: 500px) {
+        .image-gallery {
+            column-count: 3;
+        }
+
+        .content-wrapper {
+            padding: var(--spacing-m);
+        }
+
+        .description {
+            font-size: 0.9rem;
+        }
+
+        .title {
+            font-size: 1.2rem;
+        }
+
+        .title {
+            font-size: 1.2rem;
+        }
+
+        .content-wrapper {
+            padding: var(--spacing-m);
+        }
+
+        .desc {
+            font-size: 0.8rem;
+        }
+    }
 </style>
